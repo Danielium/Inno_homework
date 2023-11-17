7b55c6e576a7053a188968cc99ee20c44c1701a1
Danielium <Lapshin_danila73@mail.ru>
2023-11-17 18:43:48 +0400

    homeworks

---
+++ homeworks/number2.py
@@ -0,0 +1,7 @@
+n = int(input('Введите число: '))
+num1 = 0
+num2 = 1
+for i  in range(n):
+    num2 = num1 + num2
+    num1 = num2 - num1
+print(num1)
---
+++ homeworks/number5.py
@@ -0,0 +1,8 @@
+words = input('Введите предложение: ').split()
+count = 0
+for word in words:
+    if word.lower() == 'кот':
+        if word == word.upper() or word == word.lower() or word == word.capitalize():
+            count += 1
+print(f'кот/Кот/КОТ встречается {count} раз(а)')
+
