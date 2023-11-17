e53bb218a98c72630735c87ef1cd9cbec7479559
Danielium <Lapshin_danila73@mail.ru>
2023-11-17 18:56:36 +0400

    number 5

--- homeworks/number5.py
+++ homeworks/number5.py
@@ -4,5 +4,5 @@ for word in words:
     if word.lower() == 'кот':
         if word == word.upper() or word == word.lower() or word == word.capitalize():
             count += 1
-print(f'кот/Кот/КОТ встречается {count} раз(а)')
+print(f'кот/Кот/КОТ встречается в предложении {count} раз(а)')
 
