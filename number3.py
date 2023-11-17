dc3c14a3bb38af8874a62bbc674a713d4aae74f4
Danielium <Lapshin_danila73@mail.ru>
2023-11-17 17:55:11 +0400

    number 3

---
+++ number3.py
@@ -0,0 +1,16 @@
+global_var = 10 #Глобальная переменная
+
+
+def calculate_sum(param): # Параметр функции
+    outer_var = 5 # Переменная из охватывающей области видимости
+
+    def inner_function():
+        nonlocal outer_var
+        outer_var = 8
+        local_var = 7 # Локальная переменная
+        return global_var + outer_var + param + local_var
+
+    return inner_function()
+
+
+print(calculate_sum(3))
