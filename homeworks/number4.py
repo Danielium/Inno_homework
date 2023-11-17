48c87b353be2f0579c85792d3f0b24f83c2ee9b7
Danielium <Lapshin_danila73@mail.ru>
2023-11-17 22:24:27 +0400

    number 4

--- homeworks/number4.py
+++ homeworks/number4.py
@@ -1,1 +1,9 @@
-#Ещё не сделано
+def menu(operation):
+    match operation:
+        case 'save':
+            print('Сохранить')
+        case 'load':
+            print('Загрузить')
+        case _:
+            print('Неизвестная операция')
+menu((input('Введите операцию: ')))
