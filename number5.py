4d0a92e01b5b0511d05d4157e7b6e8b304a9cf2b
Danielium <Lapshin_danila73@mail.ru>
2023-11-17 18:59:38 +0400

    added folders

--- number5.py
+++
@@ -1,8 +0,0 @@
-words = input('Введите предложение: ').split()
-count = 0
-for word in words:
-    if word.lower() == 'кот':
-        if word == word.upper() or word == word.lower() or word == word.capitalize():
-            count += 1
-print(f'кот/Кот/КОТ встречается {count} раз(а)')
-
